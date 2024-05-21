c95ea1d5d101b08e309d36a0ca6212971e5691b3
Roland Tanglao <roland@rolandtanglao.com>
2024-05-20 21:04:19 -0700

    appears to be creating filename

--- get-thumbs-for-1hour-and-make-vertical-infographic.rb
+++ get-thumbs-for-1hour-and-make-vertical-infographic.rb
@@ -55,8 +55,10 @@ YYYY = ARGV[0]
 MM = ARGV[1]
 DD = ARGV[2]
 HH = ARGV[3]
-
+# THUMBS/2024-05-20
+THUMBS_PATH = "THUMBS/#{format('%4.4d', YYYY.to_i)}-#{format('%2.2d', MM.to_i)}-#{format('%2.2d', DD.to_i)}"
 flickr_config = ParseConfig.new('flickr.conf').params
+
 api_key = flickr_config['api_key']
 
 BEGIN_TIME = Time.parse "#{YYYY} #{MM}/#{DD} #{HH}:00 -0800"
@@ -78,39 +80,85 @@ logger.debug "begin_mysql_time:#{begin_mysql_time}"
 
 NUM_PHOTOS_TO_DOWNLOAD = 500
 
-url_params =
-  {
-    method: 'flickr.photos.search',
-    media: 'photos', # Just photos no videos
-    content_type: 1, # Just photos, no videos, screenshots, etc
-    api_key: api_key,
-    format: 'json',
-    nojsoncallback: '1',
-    extras: extras_str,
-    sort: 'date-taken-asc',
-    per_page: NUM_PHOTOS_TO_DOWNLOAD,
-    page: 1,
-    # Looks like unix time support is broken so use mysql time
-    min_taken_date: begin_mysql_time,
-    max_taken_date: end_mysql_time
-  }
-photos_on_this_page = get_flickr_response(flickr_url, url_params, logger)
-logger.debug "STATUS from flickr API:#{photos_on_this_page['stat']} num_pages:\
-  #{photos_on_this_page['photos']['pages'].to_i}"
-binding.pry
-PARAMS_TO_KEEP = %w[id dateupload url_l height_l width_l]
-photos = []
-photos_on_this_page['photos']['photo'].each do |photo|
-  # logger.debug "photo from API: #{photo.ai}"
-  dateupload = Time.at(photo['dateupload'].to_i)
-  logger.debug "dateupload:#{dateupload}"
-  photo['id'] = photo['id'].to_i
-  photo['dateupload'] = photo['dateupload'].to_i
-  next if !photo.has_key?('height_l') || photo['height_l'] < 640 # Skip all photos that are less than 640px high.
+page = 1
+photos_to_retrieve = NUM_PHOTOS_TO_DOWNLOAD
+first_page = true
+csv_array = []
+photo_index = 0
+while photos_to_retrieve.positive?
+  url_params =
+    {
+      method: 'flickr.photos.search',
+      media: 'photos', # Just photos no videos
+      content_type: 1, # Just photos, no videos, screenshots, etc
+      api_key: api_key,
+      format: 'json',
+      nojsoncallback: '1',
+      extras: extras_str,
+      sort: 'date-taken-asc',
+      per_page: NUM_PHOTOS_TO_DOWNLOAD,
+      page: page.to_s,
+      # Looks like unix time support is broken so use mysql time
+      min_taken_date: begin_mysql_time,
+      max_taken_date: end_mysql_time
+    }
+  photos_on_this_page = get_flickr_response(flickr_url, url_params, logger)
+  if first_page
+    first_page = false
+    photos_per_page = photos_on_this_page['photos']['perpage'].to_i
+    photos_to_retrieve = photos_on_this_page['photos']['total'].to_i - photos_per_page
+  else
+    photos_to_retrieve -= photos_per_page
+  end
+  page += 1
+  logger.debug "STATUS from flickr API:#{photos_on_this_page['stat']} retrieved page:"\
+    "#{photos_on_this_page['photos']['page']} of:#{photos_on_this_page['photos']['pages'].to_i}"
 
-  photo_without_unnecessary_stuff = photo.slice(*PARAMS_TO_KEEP)
-  logger.debug "photo without unnecessary stuff: #{photo_without_unnecessary_stuff.ai}"
-  photos.push(photo_without_unnecessary_stuff)
+  photos_on_this_page['photos']['photo'].each do |photo|
+    photo_index += 1
+    logger.debug "PHOTO datetaken from flickr API: #{photo['datetaken']}"
+    logger.debug "PHOTO dateupload from flickr API: #{photo['dateupload']}"
+
+    skip = false
+    begin
+      datetaken = Time.parse(photo['datetaken'])
+    rescue ArgumentError
+      logger.debug "Parser EXCEPTION in datetaken: #{photo['datetaken']}!! SKIPPED"
+      skip = true
+    end
+    if skip
+      skip = false
+      next
+    end
+    datetaken = datetaken.utc
+    logger.debug "PHOTO datetaken:#{datetaken}"
+    photo['datetaken'] = datetaken
+    dateupload = Time.at(photo['dateupload'].to_i)
+    logger.debug "PHOTO dateupload: #{dateupload}"
+    photo['dateupload'] = dateupload
+    lastupdate = Time.at(photo['lastupdate'].to_i)
+    logger.debug "PHOTO lastupdate:#{lastupdate}"
+    photo['lastupdate'] = lastupdate
+    photo['id'] = photo['id'].to_i
+    id = photo['id']
+    logger.debug "PHOTO id: #{id}"
+    logger.debug "photo.ai: #{photo.ai}"
+    photo['description_content'] = photo['description']['_content']
+    photo['thumbs_path'] = THUMBS_PATH
+    # filename format:     # 000001-2024-01-01-01-owner-id-title-75x75
+    thumb_filename = "#{format('%6.6d', photo_index)}-"\
+    "#{format('%4.4d', YYYY.to_i)}-#{format('%2.2d', MM.to_i)}-"\
+    "#{format('%2.2d', DD.to_i)}-#{format('%2.2d', HH.to_i)}-"\
+    "#{photo['owner'].gsub('@', '_')}"\
+    "-#{photo['id']}-#{photo['title'].gsub(/[^\w.]/, '_')[0..31]}-75x75.jpg"
+    logger.debug "thumb_filename:#{thumb_filename}"
+    photo['thumb_filename'] = thumb_filename
+    photo_without_nested_stuff = photo.except('description')
+    csv_array.push(photo_without_nested_stuff)
+    # logger.debug photo.except("description").ai
+    logger.debug "photo_without_nested_stuff #{photo_without_nested_stuff.ai}"
+    binding.pry
+  end
 end
 photos.sort! { |a, b| a['dateupload'] <=> b['dateupload'] }
 # Get last photo and figure out the date for the Pacific timezone
@@ -123,7 +171,7 @@ localmm = localtime.strftime('%m').to_i
 localdd = localtime.strftime('%d').to_i
 startdate = tz.local_time(localyyyy, localmm, localdd, 0, 0).to_i
 photos.reject! { |p| p['dateupload'] < startdate }
-exit if photos.length.zero?
+exit if photos.length.empty?
 BARCODE_SLICE = '/tmp/resized.png'
 HEIGHT = 640
 WIDTH = 1
