b53dd11169b8469426a74f8efa73dd93bca8730b
Roland Tanglao <roland@rolandtanglao.com>
2024-06-01 20:23:09 -0700

    try 2 works because sorting by integer fails if the int, in this case the flickr id, is greater than ruby's maxint

--- sorted-asc-try2-generate-hourly-info-graphic.rb
+++ sorted-asc-try2-generate-hourly-info-graphic.rb
@@ -12,10 +12,10 @@ require 'logger'
 require 'rmagick'
 require 'pry'
 require 'mini_magick'
-require 'pry'
 require 'pry-byebug'
 require 'down/http'
 require 'rake'
+require 'securerandom'
 
 logger = Logger.new($stderr)
 logger.level = Logger::DEBUG
@@ -39,18 +39,15 @@ relevant_metadata = []
 CSV.foreach(metadata_filename, headers: true) do |row|
   id = row['id'].to_i
   url_sq = row['url_sq']
-  #   if relevant_metadata.any? { |m| m[:url_sq] == url_sq }
-  #    logger.debug "FOUND duplicate of id: #{id} SKIPPING URL: #{url_sq}"
-  #    next
-  #  end
   relevant_metadata.push({ id: id,
                            url_sq: url_sq,
                            thumb_filename: row['thumb_filename'] })
-  logger.debug "last: #{relevant_metadata.last}"
+  # logger.debug "last: #{relevant_metadata.last}"
 end
 length = relevant_metadata.length
 logger.debug "length: #{length}"
-LARGEST_NUMBER_OF_HOURLY_PHOTOS = 209_999 # Magic number based on the fact that 215,000 photos were uploaded in 1 hour on Christmas Day 2023
+# Magic number based on the fact that 215,000 photos were uploaded in 1 hour on Christmas Day 2023
+LARGEST_NUMBER_OF_HOURLY_PHOTOS = 209_999 
 MAX_SAMPLE_SIZE = 4000
 sample_size = if length > LARGEST_NUMBER_OF_HOURLY_PHOTOS
                 MAX_SAMPLE_SIZE
@@ -59,14 +56,15 @@ sample_size = if length > LARGEST_NUMBER_OF_HOURLY_PHOTOS
               end
 logger.debug "sample_size: #{sample_size}"
 sampled_relevant_metadata = relevant_metadata.sample(sample_size)
-sorted_sampled_relevant_metadata = sampled_relevant_metadata.sort { |a, b| a[:id] <=> b[:id] }
-binding.pry
-output_filename = "#{File.basename(metadata_filename)}".ext('png')
+# flickr ids > ruby's maximum integer so sort based on a string instead of the integer id
+sampled_relevant_metadata.sort_by! { |a| a[:thumb_filename] }
+output_filename = "ascending-#{File.basename(metadata_filename)}".ext('png')
 output_filename = output_filename.gsub('metadata', 'average-colour')
-logger.debug "filename: #{output_filename}"
-AVERAGE_COLOUR_FILENAME = 'average_colour.png'
-binding.pry
+logger.debug "output_filename: #{output_filename}"
+AVERAGE_COLOUR_FILENAME = "average_colour-#{SecureRandom.hex}.png"
+logger.debug "average colour filename: #{AVERAGE_COLOUR_FILENAME}"
 sampled_relevant_metadata.each.with_index do |m, i|
+  logger.debug "at index: #{i} uniq count :#{sampled_relevant_metadata.uniq.length}"
   logger.debug "DOWNLOADING id: #{m[:id]}, url: #{m[:url_sq]}"
   skip = false
   begin
@@ -86,4 +84,8 @@ sampled_relevant_metadata.each.with_index do |m, i|
     append_image(AVERAGE_COLOUR_FILENAME, output_filename, VERTICAL)
   end
   File.delete(tempfile.path)
+  if (i % 100).zero?
+    sleep(2) # sleep 2 seconds every 100 photos
+  end
 end
+File.delete(AVERAGE_COLOUR_FILENAME)
